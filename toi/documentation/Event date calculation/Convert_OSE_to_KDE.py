09d3136a64913ffa8526275b80f8a392f5da347c
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2022-02-11 19:28:15 +0100

    Master procrastinator

--- toi/documentation/Event date calculation/Convert_OSE_to_KDE.py
+++ toi/documentation/Event date calculation/Convert_OSE_to_KDE.py
@@ -45,7 +45,7 @@ def date_calc(event_year, event_month, event_day, start_year = 1858, s_m = 1, s_
 	if start_year == 1857:
 		KDE_scripted_effect = "on_startup"
 	else:
- 		KDE_scripted_effect = "KDE_bi_yearly_event_fire_" + str(start_year)
+		KDE_scripted_effect = "KDE_bi_yearly_event_fire_" + str(start_year)
 
 	return int(days), KDE_scripted_effect
 
