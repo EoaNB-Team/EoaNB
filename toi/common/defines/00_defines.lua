a636d309cab2c4ac9065eb13bec588c97fe4bf87
Garion132 <wizzardgarion132@gmail.com>
2022-03-26 17:26:25 +1300

    tech refining

--- toi/common/defines/00_defines.lua
+++ toi/common/defines/00_defines.lua
@@ -521,7 +521,7 @@ NDefines = {
 		MAX_SUBTECHS = 3,						-- Max number of sub technologies a technology can have.
 		BASE_RESEARCH_POINTS_SAVED = 30.0,		-- Base amount of research points a country can save per slot.
 		BASE_YEAR_AHEAD_PENALTY_FACTOR = 2,		-- Base year ahead penalty
-		BASE_TECH_COST = 100,					-- Base cost for a tech. multiplied with tech cost and ahead of time penalties
+		BASE_TECH_COST = 1000,					-- Base cost for a tech. multiplied with tech cost and ahead of time penalties
 		MAX_TECH_SHARING_BONUS = 0.5, 			-- Max technology sharing bonus that can be applied instantly
 		LICENSE_PRODUCTION_TECH_BONUS = 0.2,	-- License production tech bonus
 
