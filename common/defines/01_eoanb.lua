b2de28c7732e5c268748133a1639464a72c84322
Just_A_Shawn <52894956+Just-A-Shawn@users.noreply.github.com>
2026-09-12 15:48:15 -0700

    More fixes and cleaning

--- common/defines/01_eoanb.lua
+++ common/defines/01_eoanb.lua
@@ -1,11 +1,8 @@
-Nlua = {
-	NTopbar = {
-		GAME_SPEED_LIMIT = 0,	-- Unlocks Speed to match as much as the proccessor can handle
-		GAME_SPEED_STEPS = 5,	-- DONT CHANGE -- Deals with graphics and speed settings
-		GAME_SPEED_ONE = 1,	-- DONT CHANGE --
-		GAME_SPEED_TWO = 2,	-- DONT CHANGE --
-		GAME_SPEED_THREE = 3,	-- DONT CHANGE --
-		GAME_SPEED_FOUR = 4,	-- DONT CHANGE --
-		GAME_SPEED_FIVE = 5,	-- DONT CHANGE --
-	}
-}
+NDefines.NTopbar.GAME_SPEED_LIMIT = 0,	-- Unlocks Speed to match as much as the proccessor can handle
+NDefines.NTopbar.GAME_SPEED_STEPS = 5,	-- DONT CHANGE -- Deals with graphics and speed settings
+NDefines.NTopbar.GAME_SPEED_ONE = 1,	-- DONT CHANGE --
+NDefines.NTopbar.GAME_SPEED_TWO = 2,	-- DONT CHANGE --
+NDefines.NTopbar.GAME_SPEED_THREE = 3,	-- DONT CHANGE --
+NDefines.NTopbar.GAME_SPEED_FOUR = 4,	-- DONT CHANGE --
+NDefines.NTopbar.GAME_SPEED_FIVE = 5,	-- DONT CHANGE --
+NDefines.NGraphics.POLITICAL_GRID_SMALL_BOX_LIMIT = 50, -- Limit for gridbox in political view before it will be replaced with extended gridbox
